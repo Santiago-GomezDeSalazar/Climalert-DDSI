1f9f7d1439b3bc77daec3ef0c4ac1c9f3cfe5703
Santiago-GomezDeSalazar <sgomezdesalazar@frba.utn.edu.ar>
2026-06-29 12:33:20 -0300

    Primer commit

---
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/model/entities/Weather.java
@@ -0,0 +1,11 @@
+package ar.edu.utn.ba.ddsi.climalert.model.entities;
+
+import java.time.LocalDateTime;
+
+public class Weather {
+  private Double temperatura;
+  private Integer humedad;
+  private String localidad;
+  private LocalDateTime fechaHora;
+  private String estadoClima;
+}
